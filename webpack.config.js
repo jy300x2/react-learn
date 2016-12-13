b8322e845463a0eaaf68c79cbc391dc789fc3727
Jon Yu <jy300x2@gmail.com>
2016-12-13 10:15:48 -0500

    finished UI changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/app/index.html',
   filename: 'index.html',
   inject: 'body'
-})
+});
 
 module.exports = {
   entry: [
@@ -11,12 +12,13 @@ module.exports = {
   ],
   output: {
     path: __dirname + '/dist',
-    filename: 'index_bundle.js'
+    filename: "index_bundle.js"
   },
   module: {
-    loaders : [
-      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+    loaders: [
+      {test: /\.js$/, include: __dirname + '/app', loader: "babel-loader"},
+      { test: /\.css$/, loader: "style-loader!css-loader" }
     ]
   },
-  plugins : [HtmlWebpackPluginConfig]
-}
+  plugins: [HTMLWebpackPluginConfig]
+};
